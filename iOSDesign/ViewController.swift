035140b8ba0f628b5f20c03ba2d852e2895ad9a5
yusufakbs <yusufakbas@Yusuf-MacBook-Pro.local>
2023-12-21 01:57:46 +0300

    Design, AutoLayout, Align, Pin, Constraint, Multi-screen support, Multi-language support, Application icon, Application name

--- iOSDesign/ViewController.swift
+++ iOSDesign/ViewController.swift
@@ -11,7 +11,18 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.navigationItem.title = "Pizza"
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.backgroundColor = UIColor(named: "primaryColorX")
+        appearance.titleTextAttributes = [.foregroundColor: UIColor(named: "textColor")!,                                   .font:UIFont(name: "Pacifico-Regular", size: 22)!]
+        navigationController?.navigationBar.barStyle = .black
+        navigationController?.navigationBar.standardAppearance = appearance
+        navigationController?.navigationBar.compactAppearance = appearance
+        navigationController?.navigationBar.scrollEdgeAppearance = appearance
+        
+        
     }
 
 

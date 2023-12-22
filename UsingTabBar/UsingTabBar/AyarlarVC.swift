d0175424e4c52786d70bb4c5c7c521a778908f28
yusufakbs <yusufakbas@Yusuf-MacBook-Pro.local>
2023-12-22 16:19:19 +0300

    Navigation and TabBar

---
+++ UsingTabBar/UsingTabBar/AyarlarVC.swift
@@ -0,0 +1,19 @@
+//
+//  AyarlarVC.swift
+//  UsingTabBar
+//
+//  Created by Yusuf Akbaş on 22.12.2023.
+//
+
+import UIKit
+
+class AyarlarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
---
+++ UsingTabBar/UsingTabBar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UsingTabBar
+//
+//  Created by Yusuf Akbaş on 22.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
